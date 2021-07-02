5dfc2a66d62324ae82d5386e909172ae47ba0635
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-02 07:17:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,31 +2,34 @@ const toDoList = document.getElementsByClassName("to-do-list")[0];
 const input = document.getElementById("input");
 const trashIcon = document.getElementById("trash");
 
-input.addEventListener("keydown", function(event){
-  if(event.key === "Enter"){
+input.addEventListener("keydown", (event) => {
+  if(event.key === "Enter")
     addItem();
-  }
 });
 
-function addItem(){
+document.getElementById("addItem").addEventListener("click", () => {
+  addItem();
+});
+
+addItem = () => {
   var divParent = document.createElement("div");
   var divChild = document.createElement("div");
   var checkIcon = document.createElement("i");
   var trashIcon = document.createElement("i");
 
   divParent.className = "item";
-  divParent.innerHTML = '<div> <p>'+input.value+'</p> </div>';
+  divParent.innerHTML = '<div><p>'+input.value+'</p></div>';
 
   checkIcon.className = "fas fa-check-square";
   checkIcon.style.color = "lightgray";
-  checkIcon.addEventListener("click", function(){
+  checkIcon.addEventListener("click", () => {
     checkIcon.style.color = "limegreen";
   })
   divChild.appendChild(checkIcon);
 
   trashIcon.className = "fas fa-trash";
   trashIcon.style.color = "darkgray";
-  trashIcon.addEventListener("click", function(){
+  trashIcon.addEventListener("click", () => {
     divParent.remove();
   })
   divChild.appendChild(trashIcon);
